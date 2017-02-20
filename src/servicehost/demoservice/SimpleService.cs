1aa24b26279c4b9ee3579c7b682e456c4cf307d4
Ralf Westphal <info@ralfw.de>
2017-02-20 22:30:43 +0100

    Started README with demo service

---
+++ src/servicehost/demoservice/SimpleService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Script.Serialization;
+using servicehost.contract;
+
+namespace demoservice
+{
+    public class AddRequest { 
+        public int A { get; set; }
+        public int B { get; set; }
+    }
+
+    public class AddResult {
+        public int Sum { get; set; }
+    }
+
+    [Service]
+    public class SimpleService
+    {
+        [EntryPoint(HttpMethods.Get, "/add", InputSources.Querystring)]
+        public string Add(string input) {
+            var json = new JavaScriptSerializer();
+            AddRequest req = json.Deserialize<AddRequest>(input);
+
+            var simplemath = new SimpleMath();
+            var sum = simplemath.Add(req.A, req.B);
+
+            var result = new AddResult { Sum = sum };
+            return json.Serialize(result);
+        }
+    }
+
+
+    class SimpleMath {
+        public int Add(int a, int b) { return a + b; }
+    }
+}
