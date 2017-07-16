dd4e543a5fcdd3616c048fe9df9842c8337f2d66
Mike Bild <mike.bild@googlemail.com>
2017-07-16 04:49:28 +0200

    Enable unix signal handling (https://12factor.net)

--- src/servicehost/servicehost/Program.cs
+++ src/servicehost/servicehost/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Mono.Unix;
+using Mono.Unix.Native;
 
 namespace servicehost
 {
@@ -11,9 +13,37 @@ namespace servicehost
             {
                 servicehost.Start(cli.Endpoint);
 
-                Console.WriteLine("Service host running...");
-                Console.ReadLine();
+                Console.WriteLine("Service host running on " + cli.Endpoint.ToString());
+
+                if (IsRunningOnMono())
+                {
+                    var terminationSignals = GetUnixTerminationSignals();
+                    UnixSignal.WaitAny(terminationSignals);
+                }
+                else
+                {
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("Stopping service host");
+                servicehost.Stop();
             }
+        }
+
+        private static bool IsRunningOnMono()
+        {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+
+        private static UnixSignal[] GetUnixTerminationSignals()
+        {
+            return new[]
+            {
+              new UnixSignal(Signum.SIGINT),
+              new UnixSignal(Signum.SIGTERM),
+              new UnixSignal(Signum.SIGQUIT),
+              new UnixSignal(Signum.SIGHUP)
+            };
         }
     }
 }
