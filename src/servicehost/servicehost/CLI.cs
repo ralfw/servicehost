29c1c20b84d4584bac3575c9c716e14fe71e21a4
Ralf Westphal <info@ralfw.de>
2017-11-29 13:19:35 +0100

    v2.5.0: endpoint can be passed as env var
    SERVICEHOST_ENDPOINT

--- src/servicehost/servicehost/CLI.cs
+++ src/servicehost/servicehost/CLI.cs
@@ -9,8 +9,14 @@ namespace servicehost
 
         public CLI(string[] args) {
             if (args.Length < 1)
-                throw new ApplicationException("Missing endpoint address! Usage example: servicehost.exe http://localhost:1234");
-            this.Endpoint = new Uri(args[0]);
+            {
+                var envEndpoint = Environment.GetEnvironmentVariable("SERVICEHOST_ENDPOINT");
+                if (envEndpoint == null) throw new ApplicationException("Missing endpoint address! Usage example: servicehost.exe http://localhost:1234");
+                
+                this.Endpoint = new Uri(envEndpoint);
+            }
+            else
+                this.Endpoint = new Uri(args[0]);
         }
     }
 }
