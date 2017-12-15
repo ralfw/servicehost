0246cfb79ec2130529550f5ce36241e9aaf57414
Ralf Westphal <info@ralfw.de>
2017-12-15 08:05:36 +0100

    Extracted Server class
    Starting the servicehost in your app is now easier than ever:
    
    servicehost.Server.Run(new Uri(...));
    
    This will stop your app and output a message on the console.

--- src/servicehost/servicehost/Program.cs
+++ src/servicehost/servicehost/Program.cs
@@ -1,40 +1,10 @@
-﻿using System;
-using Mono.Unix;
-using Mono.Unix.Native;
-
-namespace servicehost
+﻿namespace servicehost
 {
-    class MainClass
+    public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) {
             var cli = new CLI(args);
-            using (var servicehost = new ServiceHost())
-            {
-                servicehost.Start(cli.Endpoint);
-                Console.WriteLine("Service host running on {0}...", cli.Endpoint.ToString());
-
-                if (Is_running_on_Mono) {
-                    Console.WriteLine("Ctrl-C to stop service host");
-                    UnixSignal.WaitAny(UnixTerminationSignals);
-                }
-                else {
-                    Console.WriteLine("ENTER to stop service host");
-                    Console.ReadLine();
-                }
-
-                Console.WriteLine("Stopping service host");
-                servicehost.Stop();
-            }
+			Server.Run(cli.Endpoint);
         }
-
-        private static bool Is_running_on_Mono => Type.GetType("Mono.Runtime") != null;
-
-        private static UnixSignal[] UnixTerminationSignals =>  new[] {
-			new UnixSignal(Signum.SIGINT),
-			new UnixSignal(Signum.SIGTERM),
-			new UnixSignal(Signum.SIGQUIT),
-			new UnixSignal(Signum.SIGHUP)
-		};
     }
 }
